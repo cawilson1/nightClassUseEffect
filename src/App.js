2d76720d6deeb54451b35ef985d15d991dc82712
cawilson1 <cawilson1@g.cofc.edu>
2020-04-27 19:21:31 -0400

    use useEffect to make axios API call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
+
+//https://hn.algolia.com/api/v1/search?query=browser
 
 function App() {
+  const [articles, setArticles] = useState([]);
+  const [toggle, setToggle] = useState(true);
+  const [query, setQuery] = useState("browser");
+  console.log(query);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const response = await axios.get(
+          `https://hn.algolia.com/api/v1/search?query=${query}`
+        );
+        // console.log(response);
+        //   console.log(response.data);
+        console.log(response.data.hits);
+        setArticles(response.data.hits);
+      } catch (err) {
+        console.log(err);
+      }
+      //console.log(toggle);
+      //setArticles("Article was set");
+    }
+    getData();
+  }, [query]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input onChange={event => setQuery(event.target.value)} />
+        <button onClick={() => setToggle(!toggle)}>Run Articles</button>
+        <p>{query}</p>
+        <div>
+          {articles.map(article => {
+            return <div key={article.objectID}>{article.title}</div>;
+          })}
+        </div>
       </header>
     </div>
   );
